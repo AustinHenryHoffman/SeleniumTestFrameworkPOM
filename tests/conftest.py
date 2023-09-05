039ddb883323f1edfbb48f6a1715428882ba7308
DrTautology <austinhenryhoffman@gmail.com>
2023-09-05 18:33:35 -0500

    Possibly switching over to pytest instead of unittest so that I can use external arguments to specify the browser that will be used during test runs.

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+# tests/conftest.py
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from pages.login_page import LoginPage
+
+
+@pytest.fixture(scope="class")
+def driver(request):
+    selected_browser = request.config.getoption("--browser", default="chrome")
+
+    if selected_browser == "chrome":
+        chromedriver_path = "C:\\Users\\Dr.Tautology\\PycharmProjects\\SeleniumTestFrameworkPOM\\drivers" \
+                           "\\chromedriver_116.exe "
+        service = ChromeService(executable_path=chromedriver_path)
+        options = ChromeOptions()
+        driver_instance = webdriver.Chrome(service=service, options=options)
+    elif selected_browser == "firefox":
+        geckodriver_path = "C:\\Users\\Dr.Tautology\\PycharmProjects\\SeleniumTestFrameworkPOM\\drivers" \
+                          "\\geckodriver.exe "
+        service = FirefoxService(executable_path=geckodriver_path)
+        options = FirefoxOptions()
+        driver_instance = webdriver.Firefox(service=service, options=options)
+    else:
+        raise ValueError("Invalid browser choice")
+
+    yield driver_instance
+    driver_instance.quit()
+
+
+@pytest.fixture(scope="class")
+def login_page(driver):
+    driver.get("https://demo.guru99.com")
+    return LoginPage(driver)
