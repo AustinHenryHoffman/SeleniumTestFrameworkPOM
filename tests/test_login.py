ebc240a598880321252d4ea8d70bda3df15aa83c
DrTautology <austinhenryhoffman@gmail.com>
2023-09-05 18:32:50 -0500

    Updated test. Still trying to resolve issue with test hanging after interacting with submit button.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,7 @@
 # tests/test_login.py
 import unittest
+import datetime as dt
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,26 +10,64 @@ from locators.login_page_locators import LoginPageLocators
 from pages.login_page import LoginPage
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 
 class TestLogin(unittest.TestCase):
     def setUp(self):
-        self.chromedriverpath = "C:\\Users\\Dr.Tautology\\PycharmProjects\\SeleniumTestFrameworkPOM\\drivers" \
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--browser", choices=["chrome","firefox"], default="chrome")
+        args = parser.parse_args()
+        selected_browser = args.browser
+
+        if selected_browser == "chrome":
+            chromedriverpath = "C:\\Users\\Dr.Tautology\\PycharmProjects\\SeleniumTestFrameworkPOM\\drivers" \
                             "\\chromedriver_116.exe "
-        service = ChromeService(executable_path=self.chromedriverpath)
-        self.driver = webdriver.Chrome(service=service)
+            service = ChromeService(executable_path=chromedriverpath)
+            self.driver = webdriver.Chrome(service=service)
+        elif selected_browser == "firefox":
+            geckodriverpath = "C:\\Users\\Dr.Tautology\\PycharmProjects\\SeleniumTestFrameworkPOM\\drivers" \
+                              "\\geckodriver.exe "
+            service = FirefoxService(executable_path=geckodriverpath)
+            self.driver = webdriver.Firefox(service=service)
+        else:
+            raise ValueError("Invalid browser specified.")
+
         self.driver.get("https://demo.guru99.com")
         self.login_page = LoginPage(self.driver)
+        self.wait = WebDriverWait(self.driver, 20)
+        self.report = Path("../reports")
 
     def test_successful_login(self):
+
+        self.wait.until(EC.presence_of_element_located((By.XPATH, LoginPageLocators.submit_email_button_locator[1])))
         self.login_page.enter_email("abc@gmail.com")
-        # self.login_page.enter_password("your_password")
+        print("Login information entered.")
+
+        print("Current URL:", self.driver.current_url)
+        print("Page Title:", self.driver.title)
+
+        print("Before clicking the submit button")
         self.login_page.click_submit_button()
-        WebDriverWait(10)
+        print("After clicking the submit button")
+
+        self.wait.until(EC.invisibility_of_element(LoginPageLocators.submit_email_button_locator))
+        print("Wait for element complete.")
+        print("taking a screenshot.")
+        self.driver.save_screenshot(f"screenshot{dt.datetime.now().strftime('%m_%d_%y %I_%M_%S')}.png")
+        self.wait.until(EC.presence_of_element_located((By.XPATH, LoginPageLocators.account_table[1])))
+
+        print("Submit button pressed.")
+        print("Waiting for webdriver.")
+
         # Add assertions to verify the login was successful
         try:
-            element = WebDriverWait.until(EC.presence_of_element_located((By.XPATH, *LoginPageLocators.bank_banner)))
-            assert element.text.lower() == "guru99 bank"
+            print("Waiting for element to be present.")
+            element = self.wait.until(EC.presence_of_element_located((By.XPATH, *LoginPageLocators.bank_banner)))
+            # banner_text = self.driver.find_element(LoginPageLocators.bank_banner).get_attribute("text").lower()
+            # assert banner_text == "guru99 bank"
         except Exception as e:
             print(f"Assertion Failed:{e}")
 
