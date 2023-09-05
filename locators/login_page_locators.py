ce9996cff9943d8e8532e85067ab081cc8d5c91c
AustinHenryHoffman <austinhenryhoffman@gmail.com>
2023-09-05 18:32:08 -0500

    Added new locators.

--- locators/login_page_locators.py
+++ locators/login_page_locators.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators:
     emailID_locator = (By.NAME, "emailid")
     password_locator = (By.ID, "password")
-    # login_button_locator = (By.ID, "login-button")
-    submit_email_button_locator = (By.NAME, "btnLogin")
+    submit_email_button_locator = (By.XPATH, "//input[@name='btnLogin']")
     bank_banner = (By.XPATH, "/html/body/div[2]/h2")
+    account_table = (By.XPATH, "/html/body/table")
+
