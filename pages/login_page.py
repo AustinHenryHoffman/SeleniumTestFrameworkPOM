e95c00e53628feaefe59661a7416aebb3e52e621
DrTautology <austinhenryhoffman@gmail.com>
2023-09-05 22:55:19 -0500

    Added WebDriverWait

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,11 @@
-# pages/login_page.py
 from locators.login_page_locators import LoginPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)  # Initialize WebDriverWait here
 
     def enter_email(self, email):
         self.driver.find_element(*LoginPageLocators.emailID_locator).send_keys(email)
