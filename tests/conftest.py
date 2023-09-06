389190ed9c6d44e4757b9f3355dcd6cd8c32a64b
AustinHenryHoffman <austinhenryhoffman@gmail.com>
2023-09-05 23:29:10 -0500

    Added reports_path fixture. Changed desired location for geckodriver.log.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from pages.login_page import LoginPage
+from pathlib import Path
+import os
 
 
 @pytest.fixture(scope="class")
@@ -20,7 +22,11 @@ def driver(request):
     elif selected_browser == "firefox":
         geckodriver_path = "C:\\Users\\Dr.Tautology\\PycharmProjects\\SeleniumTestFrameworkPOM\\drivers" \
                           "\\geckodriver.exe"
-        service = FirefoxService(executable_path=geckodriver_path)
+        # Attempted to redirect the location of the geckodriver.log file
+
+        # Set the desired geckodriver.log file path
+        log_file_path = str(Path("../reports/geckodriver.log"))
+        service = FirefoxService(executable_path=geckodriver_path, log_path=log_file_path)
         options = FirefoxOptions()
         driver_instance = webdriver.Firefox(service=service, options=options)
     else:
@@ -38,3 +44,12 @@ def login_page(driver):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", choices=["chrome", "firefox"], default="chrome", help="Specify the browser to use for tests (e.g., --browser chrome)")
+
+
+@pytest.fixture(scope="session")
+def reports_path():
+    # Define the path to the reports directory
+    reports_dir = Path("../reports")  # Modify the path as needed
+    reports_dir.mkdir(parents=True, exist_ok=True)
+    yield reports_dir
+
