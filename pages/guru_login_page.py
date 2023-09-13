8f9f09916a2639b5dc0390124ef6ea8810085c08
AustinHenryHoffman <austinhenryhoffman@gmail.com>
2023-09-12 21:31:45 -0500

    Moved the config parsing and login actions over from conftest.py. This encapsulates the interactions with the guru login page and allows for a more structured, modular and reusable design.

--- pages/guru_login_page.py
+++ pages/guru_login_page.py
@@ -1,11 +1,14 @@
-from locators.login_page_locators import LoginPageLocators
+from locators.guru_login_page_locators import LoginPageLocators
 from selenium.webdriver.support.ui import WebDriverWait
+import json
 
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 20)  # Initialize WebDriverWait here
+        with open("../config/config.json", 'r') as config_file:
+            self.config = json.load(config_file)
 
     def enter_email(self, email):
         self.driver.find_element(*LoginPageLocators.emailID_locator).send_keys(email)
@@ -15,3 +18,8 @@ class LoginPage:
 
     def click_submit_button(self):
         self.driver.find_element(*LoginPageLocators.submit_email_button_locator).click()
+
+    def login(self):
+        base_url = self.config["base_url"]
+        self.driver.get(base_url)
+        return LoginPage(self.driver)
